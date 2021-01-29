77c06f3d265bd451601b9576e87320fa03cdec1d
Ondrej <panicek.ondrej@gmail.com>
2021-01-29 20:56:10 +0100

    pridany chvost, kolizie so sebou samym, ukoncenie kurzu

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 // listeners
 document.addEventListener("keydown", keyPush)
 
+
 // canvas
 const canvas = document.querySelector("canvas");
 const title = document.querySelector("h1")
 const cntxt = canvas.getContext("2d");
 
+// game
+let gameIsRunning = true;
+const fps = 10;
+const tileSize = 50;
+const tileCountX = canvas.width / tileSize;
+const tileCountY = canvas.height / tileSize;
+let snakeLength = 3;
+let score = 0;
+
 // player
-const snakeSize = 50;
 let snakePosX = 0;
 let snakePosY = canvas.height / 2;
-let snakeSpeed = snakeSize;
-let velocityX = 0;
+let snakeSpeed = tileSize;
+let velocityX = 1;
 let velocityY = 0;
+
+let tail = []
 
 // fooood
 let foodPosX = 0;
 let foodPosY = 0;
 
-// game
-const tileCountX = canvas.width / snakeSize;
-const tileCountY = canvas.height / snakeSize;
-let score = 0;
 // loop
 function gameLoop() {
-	drawStuff();
-	moveStuff();
-	setTimeout(gameLoop, 1000/10)
+	if (gameIsRunning) {
+		drawStuff();
+		moveStuff();
+		setTimeout(gameLoop, 1000/fps)
+	}
 }
 
 resetFood();
@@ -38,22 +47,34 @@ gameLoop();
 		snakePosY += snakeSpeed * velocityY;
 
 		// wall collision
-		if (snakePosX + snakeSize > canvas.width) {
+		if (snakePosX + tileSize > canvas.width) {
 			snakePosX = 0;
 		}
 		if (snakePosX < 0 ) {
 			snakePosX = canvas.width
 		}
-		if (snakePosY + snakeSize > canvas.height) {
+		if (snakePosY + tileSize > canvas.height) {
 			snakePosY = 0;
 		}
 		if (snakePosY < 0 ) {
 			snakePosY = canvas.height
 		}
 
+		tail.forEach((snakePart) => {
+			if (snakePosX === snakePart.x && snakePosY === snakePart.y){
+				gameOver();
+			}
+		});
+
+		// tail
+		tail.push({x: snakePosX, y: snakePosY})
+
+		tail = tail.slice(-1 * snakeLength);
+
 		// food collision
 			if (snakePosX === foodPosX && snakePosY === foodPosY) {
 				score++;
+				snakeLength++;
 				title.textContent = score;
 				resetFood()
 			}
@@ -64,15 +85,23 @@ gameLoop();
  **/
 
 	function drawStuff() {
+
 		// background
 		rectangle("#bada55", 0, 0, canvas.width, canvas.height);
+
 		// grid
 		drawGrid();
-		// snake
-		rectangle("black", snakePosX, snakePosY, snakeSize, snakeSize);
+
 		// food
-		rectangle("orange", foodPosX, foodPosY, snakeSize, snakeSize);
-	}
+		rectangle("orange", foodPosX, foodPosY, tileSize, tileSize);
+
+		// tail
+		tail.forEach((snakePart) =>
+			rectangle('#555', snakePart.x, snakePart.y, tileSize, tileSize))
+
+			// snake
+			rectangle("black", snakePosX, snakePosY, tileSize, tileSize);
+		};
 
 	// kreslime stvorceky
 	function rectangle(color, x, y, width, height) {
@@ -82,8 +111,21 @@ gameLoop();
 
 	// randomize food position
 	function resetFood() {
-		foodPosX = Math.floor(Math.random() * tileCountX) * snakeSize;
-		foodPosY = Math.floor(Math.random() * tileCountY) * snakeSize;
+		if (snakeLength === tileCountX * tileCountY) {
+			gameOver();
+		}
+		foodPosX = Math.floor(Math.random() * tileCountX) * tileSize;
+		foodPosY = Math.floor(Math.random() * tileCountY) * tileSize;
+
+		// nedavaj jedlo na hlavu
+		if (foodPosX === snakePosX && foodPosY === snakePosY) {
+			resetFood();
+		}
+		// nedavaj jedlo na telo
+		if (tail.some(snakePart => snakePart.x === foodPosX && snakePart.y === foodPosY)){
+			resetFood();
+		}
+
 	}
 
 	/**
@@ -115,6 +157,10 @@ gameLoop();
 					velocityY = 0
 				}
 				break;
+			default:
+				// restart game
+				if (! gameIsRunning) location.reload();
+				break;
 		}
 	}
 
@@ -122,7 +168,12 @@ gameLoop();
 	function drawGrid() {
 		for (let i = 0; i < tileCountX; i++) {
 			for (let j = 0; j <tileCountY; j++){
-				rectangle("white", snakeSize * i, snakeSize * j, snakeSize - 1, snakeSize - 1);
+				rectangle("white", tileSize * i, tileSize * j, tileSize - 1, tileSize - 1);
 			}
 		}
+	}
+
+	// Game Over
+	function gameOver() {
+		gameIsRunning = false;
 	}
